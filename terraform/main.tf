dc5eb50dc18f9df02bfa47b3f62e4e9ecc6a2bba
Dulanga Lakshan <140315525+dualangalakshan002@users.noreply.github.com>
2026-01-26 01:36:24 +0530

    uuu

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,12 +17,21 @@ resource "docker_volume" "mongo_data" {
   name = "mongo_data"
 }
 
+resource "docker_image" "mongo_image" {
+  name = "mongo:6"
+}
+
 resource "docker_container" "backend" {
   name  = "canteen-backend"
   image = "dulanga002/canteen-backend:latest"
   networks_advanced {
     name = docker_network.canteen_network.name
   }
+  env = [
+    "MONGO_URI=mongodb://mongo:27017/canteen",
+    "PORT=5000"
+  ]
+  depends_on = [docker_container.mongo]
   ports {
     internal = 5000
     external = 5000
@@ -35,8 +44,21 @@ resource "docker_container" "frontend" {
   networks_advanced {
     name = docker_network.canteen_network.name
   }
+  # The frontend image serves assets (nginx) on container port 80; map it to host 5173
   ports {
-    internal = 5173
+    internal = 80
     external = 5173
   }
+}
+
+resource "docker_container" "mongo" {
+  name  = "canteen-mongo"
+  image = docker_image.mongo_image.name
+  volumes = ["${docker_volume.mongo_data.name}:/data/db"]
+  networks_advanced {
+    name = docker_network.canteen_network.name
+  }
+  env = [
+    "MONGO_INITDB_DATABASE=canteen"
+  ]
 }
