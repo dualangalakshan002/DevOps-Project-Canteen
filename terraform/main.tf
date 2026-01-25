469280160b0a7942c7ae01e9980499c68bf54114
Dulanga Lakshan <140315525+dualangalakshan002@users.noreply.github.com>
2026-01-26 01:22:38 +0530

    up tera

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,27 @@ resource "docker_network" "canteen_network" {
 resource "docker_volume" "mongo_data" {
   name = "mongo_data"
 }
+
+resource "docker_container" "backend" {
+  name  = "canteen-backend"
+  image = "dulanga002/canteen-backend:latest"
+  networks_advanced {
+    name = docker_network.canteen_network.name
+  }
+  ports {
+    internal = 5000
+    external = 5000
+  }
+}
+
+resource "docker_container" "frontend" {
+  name  = "canteen-frontend"
+  image = "dulanga002/canteen-frontend:latest"
+  networks_advanced {
+    name = docker_network.canteen_network.name
+  }
+  ports {
+    internal = 5173
+    external = 5173
+  }
+}
